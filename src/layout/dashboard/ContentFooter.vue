c983f6fafea38fe602adcf943fad46ab8a486fe5
Wells <brett.wells@uleth.ca>
2021-01-30 13:28:34 -0700

    update sidebar

--- src/layout/dashboard/ContentFooter.vue
+++ src/layout/dashboard/ContentFooter.vue
@@ -9,13 +9,7 @@
         </ul>
       </nav>
       <div class="copyright d-flex flex-wrap">
-<<<<<<< HEAD
-        &copy; Coded with
-        <i class="fa fa-heart heart"></i> by &nbsp;
-        <p style="position: relative; bottom: 3px;">uLeth pals</p>&nbsp;
-=======
         &copy; Developed by Austin, Kenish, Brett, & Matthew
->>>>>>> master
       </div>
     </div>
   </footer>
--- src/layout/dashboard/DashboardLayout.vue
+++ src/layout/dashboard/DashboardLayout.vue
@@ -5,7 +5,7 @@
         <sidebar-link to="/dashboard" name="Dashboard" icon="ti-panel"/>
         <sidebar-link to="/stats" name="User Profile" icon="ti-user"/>
         <sidebar-link to="/table-list" name="Calendar" icon="ti-view-list-alt"/>
-        <sidebar-link to="/typography" name="Typography" icon="ti-text"/>
+        <sidebar-link to="/typography" name="Daily Activity" icon="ti-text"/>
         <sidebar-link to="/icons" name="Icons" icon="ti-pencil-alt2"/>
         <sidebar-link to="/maps" name="Map" icon="ti-map"/>
         <sidebar-link to="/notifications" name="Notifications" icon="ti-bell"/>
