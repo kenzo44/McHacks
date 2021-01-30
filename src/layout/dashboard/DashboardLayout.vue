5dfbd09c7a0df3d067f0e38052f17a118ebddc81
Wells <brett.wells@uleth.ca>
2021-01-29 23:45:11 -0700

    add calendar

--- src/layout/dashboard/DashboardLayout.vue
+++ src/layout/dashboard/DashboardLayout.vue
@@ -4,7 +4,7 @@
       <template slot="links">
         <sidebar-link to="/dashboard" name="Dashboard" icon="ti-panel"/>
         <sidebar-link to="/stats" name="User Profile" icon="ti-user"/>
-        <sidebar-link to="/table-list" name="Table List" icon="ti-view-list-alt"/>
+        <sidebar-link to="/table-list" name="Calendar" icon="ti-view-list-alt"/>
         <sidebar-link to="/typography" name="Typography" icon="ti-text"/>
         <sidebar-link to="/icons" name="Icons" icon="ti-pencil-alt2"/>
         <sidebar-link to="/maps" name="Map" icon="ti-map"/>
