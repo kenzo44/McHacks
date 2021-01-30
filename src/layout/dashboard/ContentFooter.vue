8338b4f16df96b64ac25f9522b4aa90d2083ef0f
Wells <brett.wells@uleth.ca>
2021-01-30 11:45:19 -0700

    add task page and update footer

--- src/layout/dashboard/ContentFooter.vue
+++ src/layout/dashboard/ContentFooter.vue
@@ -10,9 +10,8 @@
       </nav>
       <div class="copyright d-flex flex-wrap">
         &copy; Coded with
-        <i class="fa fa-heart heart"></i> by
-        <a href="https://github.com/cristijora" target="_blank"> &nbsp; Cristi Jora.</a>&nbsp;
-        Designed by <a href="https://www.creative-tim.com/?ref=pdf-vuejs" target="_blank">&nbsp; Creative Tim.</a>
+        <i class="fa fa-heart heart"></i> by &nbsp;
+        <p style="position: relative; bottom: 3px;">uLeth pals</p>&nbsp;
       </div>
     </div>
   </footer>
