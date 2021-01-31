ae022d1c40d62746e2e98def9215d36a41e717c4
Wells <brett.wells@uleth.ca>
2021-01-30 20:54:35 -0700

    Add rough html for goals/values page

--- src/pages/TableList.vue
+++ src/pages/TableList.vue
@@ -1,8 +1,29 @@
 <template>
 	<div>
     <h1>Goals and Values</h1>
-    <p>About goals and values</p>
+    <p>"Goals are things that you are aiming for in the future: things you want to get, have, or achieve. In contrast,
+      values are how you want to behave right now and on an ongoing basis for the rest of your life, and how you want to
+       behave every step of the way toward achieving your goals; whether you achieve them or not."</p>
+
+    <div class="Values">
+      <h3>Values</h3>
+      <ul>
+        <li>List Item 1</li>
+        <li>List Item 2</li>
+        <li>List Item 3</li>
+      </ul>
+    </div>
+
+    <div class="goals">
+      <h3>Goals</h3>
+      <ul>
+        <li>List Item 1</li>
+        <li>List Item 2</li>
+        <li>List Item 3</li>
+      </ul>
+    </div>
 	</div>
+
 </template>
 
 <script>
