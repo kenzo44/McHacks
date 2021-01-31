9d9bd2ebfde4a830493c5acc8d7e5687e8f37bf9
Wells <brett.wells@uleth.ca>
2021-01-30 23:15:42 -0700

    add content and styling to goals and values

--- src/pages/TableList.vue
+++ src/pages/TableList.vue
@@ -7,20 +7,25 @@
 
     <div class="Values">
       <h3>Values</h3>
-      <ul>
-        <li>List Item 1</li>
-        <li>List Item 2</li>
+      <div class="listWrap">
+      <ul class="myLists">
+        <li>Compassion/self-compassion: to act kindly toward myself and others in pain.</li>
+        <li>Supportiveness: to be supportive, helpful, and available to myself and others.</li>
         <li>List Item 3</li>
       </ul>
+    </div>
     </div>
 
     <div class="goals">
       <h3>Goals</h3>
-      <ul>
+      <div class="listWrap">
+      <ul class="myLists">
         <li>List Item 1</li>
         <li>List Item 2</li>
         <li>List Item 3</li>
+        <li>List Item 3</li>
       </ul>
+    </div>
     </div>
 	</div>
 
@@ -33,7 +38,31 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 
+  .listWrap {
+    background: linear-gradient(#340088, #1CB5E0);
+    color: #fff;
+    padding: 2px;
+    border-radius: 5px;
+    position: relative;
+  }
+
+  .myLists {
+    list-style-type: none;
+
+    justify-content: space-around;
+    border-radius: 5px;
+    border: 3px;
+    border-color: none;
+    line-height: 30px;
+    padding: 5px;
+    background-color: white;
+
+    li {
+      color: #66615b;
+      font-size: 20px;
+    }
+  }
 
 </style>
